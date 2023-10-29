9d9f3a74a7deae7485fa0ff1b72691bedc4e0366
haggqvist459 <tim.haggqvist@gmail.com>
2023-10-29 17:54:29 +1100

    Initial Commit

---
+++ tutorial_weather_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tutorial_weather_app
+//
+//  Created by Tim Häggqvist Luotomäki on 29/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tutorial_weather_app/tutorial_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorial_weather_appApp.swift
+//  tutorial_weather_app
+//
+//  Created by Tim Häggqvist Luotomäki on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct tutorial_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
